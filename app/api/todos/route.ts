63f9567378da415276e4df58a57670718465405b
Krzysztof Kaptur <krzysztof.kaptur@gmail.com>
2024-03-15 22:20:42 +0100

    add drizzle

--- app/api/todos/route.ts
+++ app/api/todos/route.ts
@@ -1,7 +1,14 @@
+import {db} from '@/app/db/index'
+import { todos } from '@/app/db/schema'
+import { asc } from 'drizzle-orm';
+
 export async function GET(request: Request) {
-  const data = await fetch('http://localhost:5000/todos', {
-    cache: "no-store"
-  }).then(res => res.json())
+  // const data = await fetch('http://localhost:5000/todos', {
+  //   cache: "no-store"
+  // }).then(res => res.json())
+  const data = await db.query.todos.findMany({
+    orderBy: [asc(todos.id)]
+  })
 
   return Response.json(data)
 }
@@ -9,11 +16,12 @@ export async function GET(request: Request) {
 export async function POST(request: Request) {
   const body = await request.json()
 
-  fetch("http://localhost:5000/todos", {
-    method: "POST",
-    body: JSON.stringify(body)
-  })
+  // fetch("http://localhost:5000/todos", {
+  //   method: "POST",
+  //   body: JSON.stringify(body)
+  // })
+
+  await db.insert(todos).values({ title: body.title })
 
   return Response.json({})
 }
-
